d3a9d3ffbfe0ba328b95a66663a922dfcc9ad321
Luis Castillo <luised.castillo@gmail.com>
2021-10-27 20:38:37 -0500

    Add helloworkd sync sample

--- ios/CounterAppPoCTS2/HelloWorld.swift
+++ ios/CounterAppPoCTS2/HelloWorld.swift
@@ -31,7 +31,24 @@ class HelloWorld: NSObject, RCTBridgeModule, StreamDelegate {
   }
   
   @objc
-  func ShowMessage(_ message: String, duration: Double, 
+  func ShowMessageSync(_ message: NSString, duration: Double) -> Void{
+    print("NATIVE MODULE SAYING HI!!!!");
+    let alert = UIAlertController(title: nil, message: message as String, preferredStyle: .alert);
+    let seconds:Double = duration;
+    alert.view.backgroundColor = .black
+    alert.view.alpha = 0.5
+    alert.view.layer.cornerRadius = 14
+    
+    DispatchQueue.main.async {
+      (UIApplication.shared.delegate as? AppDelegate)?.window.rootViewController?.present(alert, animated: true, completion: nil)
+    }
+    
+    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds, execute: {alert.dismiss(animated: true, completion: nil)})
+
+  }
+  
+  @objc
+  func ShowMessage(_ message: String, duration: Double,
                   resolve: RCTPromiseResolveBlock, 
                   rejecter reject: RCTPromiseRejectBlock) -> Void{
     self.connectionCount = 0
